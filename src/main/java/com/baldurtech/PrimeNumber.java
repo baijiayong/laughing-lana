279f6304ae4dadb9b0e25b9f8f095fdab88cd4f2
baijiayong <jiayongbai@gmail.com>
2014-05-25 00:01:13 +0800

    增加了PrimeNumber.java并写出了一个测试用例

---
+++ src/main/java/com/baldurtech/PrimeNumber.java
@@ -0,0 +1,12 @@
+package com.baldurtech;
+
+import java.util.ArrayList;
+import java.util.List;
+ 
+public class PrimeNumber
+{
+    public ArrayList say(Integer i)
+    {
+        return null;
+    }
+}
---
+++ src/test/java/com/baldurtech/PrimeNumberTest.java
@@ -0,0 +1,10 @@
+package com.baldurtech;
+
+public class PrimeNumberTest
+{
+    public static void main(String args[])
+    {
+        PrimeNumber num = new PrimeNumber();
+        System.out.println(num.say(1));
+    }
+}
